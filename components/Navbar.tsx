2fcbe931310e33a40b0bcac4e1756a5c976b63a9
iamjunaidjutt <info.iamjunaidjutt@gmail.com>
2023-08-19 21:00:08 +0500

    add scroll functionality to navbar and modify the glassmorphism effect

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,37 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
-
 import MainNav from "@/components/MainNav";
 
 const Navbar = () => {
+	const [scrolled, setScrolled] = useState(false);
+
+	const handleScroll = () => {
+		if (window.scrollY > 0) {
+			setScrolled(true);
+		} else {
+			setScrolled(false);
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
 	return (
-		<div className="fixed w-screen lg:w-[95.7%] top-0 right-0 lg:ml-20 z-40">
-			<div className="dark:shadow-gray-800 shadow-gray-300 shadow flex items-center space-x-6 justify-between px-14 py-1 bg-blue-100 dark:bg-gray-900">
+		<div
+			className={`fixed w-screen lg:w-[95.7%] top-0 right-0 lg:ml-20 z-40 ${
+				scrolled
+					? "bg-blue-100 dark:bg-gray-900 dark:shadow-gray-800 shadow-gray-300 shadow "
+					: "bg-transparent"
+			}`}
+		>
+			<div className="flex items-center space-x-6 justify-between px-14 py-1">
 				<Link href="/">
 					<Image
 						src="/logos/logo-mini.png"
